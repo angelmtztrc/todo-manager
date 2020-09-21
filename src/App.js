9a0e15ca1d3a3b3876942c13ce1cd4a263ffab34
Angel Martinez <angel.codes139@gmail.com>
2020-09-21 10:14:15 -0500

    :construction: basic jsx structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <div className="min-h-screen bg-gray-900">
+      <div className="container mx-auto flex flex-col justify-center items-center pt-20">
+        <h1 className="text-5xl text-white font-bold">Todo Manager</h1>
+        <div>input</div>
+        <div>box with todos</div>
+      </div>
+    </div>
   );
 }
 
