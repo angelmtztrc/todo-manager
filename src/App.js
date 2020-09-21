56d9b08ae4b516efe98768da83b2cfdb094fadc0
Angel Martinez <angel.codes139@gmail.com>
2020-09-21 10:20:30 -0500

    :sparkles: adding input component and prettier

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+// Components
+import Input from './components/Input';
+
 function App() {
   return (
     <div className="min-h-screen bg-gray-900">
       <div className="container mx-auto flex flex-col justify-center items-center pt-20">
         <h1 className="text-5xl text-white font-bold">Todo Manager</h1>
-        <div>input</div>
+        <Input />
         <div>box with todos</div>
       </div>
     </div>
---
+++ src/components/Input.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Input = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Todo description" />
+    </div>
+  );
+};
+
+export default Input;
