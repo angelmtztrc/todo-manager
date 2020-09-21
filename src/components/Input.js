a7c1fee13bbc46e62ed15a8168ca587219cb26f2
Angel Martinez <angel.codes139@gmail.com>
2020-09-21 10:23:03 -0500

    :sparkles: styles for input

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Input = () => {
   return (
-    <div>
-      <input type="text" placeholder="Todo description" />
+    <div className="w-full mt-5">
+      <input
+        type="text"
+        placeholder="Todo description"
+        className="px-6 py-3 mx-auto w-full block max-w-xl rounded-md shadow-md placeholder-gray-900"
+      />
     </div>
   );
 };
