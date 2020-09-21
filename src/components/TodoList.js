d19033a051c3d0f481f6eb7fa7a989560bd64768
Angel Martinez <angel.codes139@gmail.com>
2020-09-21 12:06:19 -0500

    :sparkles: show text if no todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,9 +7,11 @@ import Todo from './Todo';
 const TodoList = ({ todos }) => {
   return (
     <main className="mt-5 mx-auto bg-white w-full max-w-lg rounded-md shadow-md p-8">
-      {todos.map(todo => (
-        <Todo todo={todo} />
-      ))}
+      {todos.length > 0 ? (
+        todos.map(todo => <Todo todo={todo} />)
+      ) : (
+        <p className="text-center text-gray-700">No todos yet</p>
+      )}
     </main>
   );
 };
