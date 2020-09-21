2f24a06d9fb0bea2711cd456e8454c0141c2b962
Angel Martinez <angel.codes139@gmail.com>
2020-09-21 10:57:56 -0500

    :bug: fix problem of properties

--- src/components/Input.js
+++ src/components/Input.js
@@ -38,16 +38,16 @@ const Input = () => {
           className="absolute top-0 right-0 mt-2 mr-6 py-1 px-2 bg-green-600 hover:bg-green-700 transition-all ease-out duration-300 rounded-md"
         >
           <svg
-            class="w-6 h-6 text-white"
+            className="w-6 h-6 text-white"
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M12 6v6m0 0v6m0-6h6m-6 0H6"
             ></path>
           </svg>
