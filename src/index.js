def1bd92830cc446d41fe13e6ac12f9fa422e9ab
Angel Martinez <angel.codes139@gmail.com>
2020-09-21 10:09:18 -0500

    :lipstick: adding styles in the entry file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Global Styles
+import './styles.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
